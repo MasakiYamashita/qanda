9df54eef43cebd54bc6350de65de0356e43043bc
MasakiYamashita <misonikomi.tech@gmail.com>
2022-08-14 10:21:30 +0900

    validation check.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,22 +1,36 @@
 class QuestionsController < ApplicationController
   # 質問一覧表示
   def index
-    
+    @questions = Question.all
+    # p "@questions : #{@questions}"
   end
 
   # 質問詳細ページ表示
   def show
-    
+    # p "paramsのid : #{params[:id]}"
+    @question = Question.find(params[:id])
+    p "@question :  #{@question}"
   end
 
   # 質問の作成
   def new
-    
+    @question = Question.new
   end
 
   # 質問の登録
   def create
-    
+    # Questionモデルを初期化
+    @question = Question.new(question_params)
+    # QuestionモデルをDBへ保存
+    if @question.save
+      # showへリダイレクト
+      # p "@question : #{@question}"
+      redirect_to @question
+    else
+      render 'new', status: :unprocessable_entity
+      # renderはテンプレートを指定することができる。'new'のテンプレートを指定。
+      # unprocessable_entityはステータスコード422。Validation Errorのときに返すべきステータス。
+    end
   end
 
   # 質問の編集
@@ -32,5 +46,10 @@ class QuestionsController < ApplicationController
   # 質問の削除
   def destroy
     
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:title, :name, :content)
   end
 end
